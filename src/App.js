f7d8dad0f8649243ffe93f44125a02810b4add57
Justin Akridge <akridjus@gmail.com>
2022-12-19 23:13:33 -0500

    added: navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+import HomeModal from './components/HomeModal';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HomeModal />
+    </div>
+  );
+}
+
+export default App;
