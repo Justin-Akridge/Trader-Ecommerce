835f9b98a18dde432e02bf3276de47c0dfd77802
Justin Akridge <akridjus@gmail.com>
2022-12-20 21:45:35 -0500

    imgs

--- src/components/HomeModal.jsx
+++ src/components/HomeModal.jsx
@@ -1,38 +1,38 @@
 import Carousel from 'react-bootstrap/Carousel';
-
-function HomeModal() {
+import slide from '../img/slide.jpg'
+function IndividualIntervalsExample() {
   return (
-    <Carousel variant="dark">
-      <Carousel.Item>
+    <Carousel>
+      <Carousel.Item interval={1000}>
         <img
-          className="d-block w-100"
-          src="./src/components/slide.jpg"
+          className="d-block w-30"
+          src={slide}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h5>First slide label</h5>
+          <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item interval={500}>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=eee"
+          className="d-block w-30"
+          src={slide}
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
+          <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+          className="d-block w-30 justify-center"
+          src={slide}
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h5>Third slide label</h5>
+          <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
@@ -42,4 +42,4 @@ function HomeModal() {
   );
 }
 
-export default HomeModal;
+export default IndividualIntervalsExample;
